55e93f7f501c0066c9effcc5620fd724ae9cb7f5
Lars Magnus <larsmagnusnordeide@gmail.com>
2026-04-10 17:33:25 +0200

    Add simple sprite rendering

---
+++ TODO.md
@@ -0,0 +1,26 @@
+### Backlog
+- [ ] Game balancing
+- [ ] Particle effects
+- [ ] Sound effects
+
+### To do
+- [ ] Enemies
+- [ ] Tilemap
+  - [ ] Path
+  - [ ] Placeable
+- [ ] Towers
+  - [ ] Attack
+  - [ ] Placement
+  - [ ] Upgrades
+- [ ] User interface
+- [ ] Waves
+- [ ] Win & loss condition
+
+### In progress
+- [ ] Add dependency: yampa
+
+### Done
+- [X] Setup dev environment
+- [X] Add dependency: gloss
+- [X] Display window and draw shapes
+- [X] Simple sprite rendering
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,32 @@
 module Main where
 
+import Graphics.Gloss
+
+type Time = Float
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = animate window background frame
+
+window :: Display
+window = InWindow "Tower Defence" (800, 600) (100, 100)
+
+background :: Color
+background = black
+
+frame :: Time -> Picture
+frame time = Pictures $
+    map (tower time) towers
+
+towers :: [Point]
+towers =
+    [ ( 0,  0)
+    , (90, 30)
+    , (30, 90)
+    ]
+
+tower :: Time -> Point -> Picture
+tower time position =
+    uncurry translate position $ Pictures
+    [ color white $ Circle 20
+    , color white $ Line [ (0, 0), (sin (time * 3.1415 / 60.0) * 20, cos (time * 3.1415 / 60.0) * 20) ]
+    ]
